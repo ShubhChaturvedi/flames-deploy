a71d6ae61c3416b83e1390d8e55b4ae7cbcba8b2
Shubh Chaturvedi <chaturvedishubh650@gmail.com>
2024-07-16 22:53:15 +0530

    learning github

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 function connectMongoDB() {
 
     mongoose.connect(process.env.MONGO_URI).then(() => {
-        console.log('Connected to MongoDB');
+        console.log('Connected to MongoDB atlas');
     }
     ).catch((error) => {
         console.log('Error:', error);
